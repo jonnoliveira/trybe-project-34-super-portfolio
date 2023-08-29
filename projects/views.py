be732d00578850fc7209d4f53cd2a053109ad646
Jonn <jonnutri@gmail.com>
2023-08-29 16:42:56 -0300

    req04 - ok

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,30 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from projects.models import Profile, Project
 from projects.serializers.profile_serializer import ProfileSerializer
 from projects.serializers.project_serializer import ProjectSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if self.request.method == "GET":
+            profile = Profile.objects.get(id=kwargs["pk"])
+            context = {
+                "profile": profile,
+                "projects": profile.projects.all(),
+            }
+
+            return render(request, "profile_detail.html", context)
+        return super().retrieve(request, id)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
